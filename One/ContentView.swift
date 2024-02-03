011702660f62a90495eaf887ba30a4e5d054f88c
Haojun Yuan <haojundev@gmail.com>
2024-02-03 14:23:46 -0500

    Initial Commit

---
+++ One/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  One
+//
+//  Created by Brian Yuan on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ One/OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneApp.swift
+//  One
+//
+//  Created by Brian Yuan on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
